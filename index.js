85c32fb5554c8bb733f13d4f49245f462d7adc76
HarshitIndira <sharshit@indiratrade.com>
2024-04-01 13:06:43 +0530

    implemented verify token(JWT) concept

--- index.js
+++ index.js
@@ -2,26 +2,47 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const app = express();
 
-const secretKey = "secretkey";
+const secretKey = "MySecretKey";
 
-app.get('/',(req,resp)=>{
-    resp.json({
-        name:"Harshit"
-    })
-})
-
-app.post('/login',(req,resp)=>{
-
+app.post('/login', (req,resp)=>{
     const user ={
         id:1,
-        name:"Mahi",
-        jersey:7
+        name:"Mahi"
+    }   
+
+    jwt.sign({user}, secretKey,{expiresIn:'300s'},(err,token)=>{
+        resp.json(token);
+    });
+});
+
+app.post('/profile',verifyToken,(req,resp)=>{
+    jwt.verify(req.token,secretKey,(err,authData)=>{
+        if(err){
+            resp.send({
+                result: "Token is invalid"
+            })
+        }else{
+            resp.json({
+                message: "Profile fetched successfully..",
+                authData
+            })
+        }
+    });
+});
+
+function verifyToken(req,resp,next){
+    const bearerHeader = req.headers['authorization'];
+    
+    if(typeof bearerHeader !== undefined){
+        const bearer = bearerHeader.split(" ");
+        const token = bearer[1];
+        req.token = token;
+        next();
+    }else{
+        resp.send({
+            result: "Token is invalid....."
+        })
     }
+}
 
-    jwt.sign({user},secretKey,{expiresIn: '300s'},(err,token)=>{
-        resp.json(token)
-        
-    })
-})
-
-app.listen(4500);
+app.listen(5000);
